41d3b5139a08a0fad8dee2d9bd29f2c9d8463751
Jan Pilař <jan.pilar@ossp.cz>
2023-10-11 13:01:06 +0200

    init

---
+++ app/Modules/Admin/Presenters/HomepagePresenter.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Admin\Presenters;
+
+use Nette\Application\UI\Presenter;
+
+final class HomepagePresenter extends Presenter
+{
+}
---
+++ app/Modules/Front/Presenters/HomepagePresenter.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Front\Presenters;
+
+use Nette\Application\UI\Presenter;
+
+final class HomepagePresenter extends Presenter
+{
+}
